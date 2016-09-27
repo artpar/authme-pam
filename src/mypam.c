72c1427a2983781fdaf9283f361be3c8d5498dd9
Parth Mudgal <artpar@gmail.com>
2016-09-28 01:38:31 +0530

    test call

--- src/mypam.c
+++ src/mypam.c
@@ -14,6 +14,65 @@ int strpos1(char *haystack, char *needle)
   return -1;
 }
 
+struct string1
+{
+  char *ptr;
+  size_t len;
+};
+
+void init_string(struct string1 *s)
+{
+  s->len = 0;
+  s->ptr = malloc(s->len + 1);
+  if (s->ptr == NULL)
+  {
+    fprintf(stderr, "malloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  s->ptr[0] = '\0';
+}
+
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string1 *s)
+{
+  size_t new_len = s->len + size * nmemb;
+  s->ptr = realloc(s->ptr, new_len + 1);
+  if (s->ptr == NULL)
+  {
+    fprintf(stderr, "realloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  memcpy(s->ptr + s->len, ptr, size * nmemb);
+  s->ptr[new_len] = '\0';
+  s->len = new_len;
+
+  return size * nmemb;
+}
+
+/*C substring function: It returns a pointer to the substring */
+
+char *substring(char *string, int position, int length)
+{
+  char *pointer;
+  int c;
+
+  pointer = malloc(length + 1);
+
+  if (pointer == NULL)
+  {
+    printf("Unable to allocate memory.\n");
+    exit(1);
+  }
+
+  for (c = 0; c < length; c++)
+  {
+    *(pointer + c) = *(string + position - 1);
+    string++;
+  }
+
+  *(pointer + c) = '\0';
+
+  return pointer;
+}
 
 /* expected hook */
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
