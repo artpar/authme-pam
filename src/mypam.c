acb6919e05de5d33daca38d5c5dfc5a00adfa2dd
Parth Mudgal <artpar@gmail.com>
2016-09-28 01:54:02 +0530

    test call

--- src/mypam.c
+++ src/mypam.c
@@ -98,44 +98,44 @@ int post1(const char *username, char *referenceId)
   curl1 = curl_easy_init();
   if (curl1)
   {
-    struct string1 s;
-    init_string(&s);
-    char *status;
+    // struct string1 s;
+    // init_string(&s);
+    // char *status;
 
-    /* First set the URL that is about to receive our POST. This URL can
-       just as well be a https:// URL if that is what should receive the
-       data. */
-    curl_easy_setopt(curl1, CURLOPT_URL, "http://authme.io/v1/trylogin");
-    /* Now specify the POST data */
-    curl_easy_setopt(curl1, CURLOPT_POSTFIELDS, str1);
-    curl_easy_setopt(curl1, CURLOPT_WRITEDATA, &s);
+    // /* First set the URL that is about to receive our POST. This URL can
+    //    just as well be a https:// URL if that is what should receive the
+    //    data. */
+    // curl_easy_setopt(curl1, CURLOPT_URL, "http://authme.io/v1/trylogin");
+    // /* Now specify the POST data */
+    // curl_easy_setopt(curl1, CURLOPT_POSTFIELDS, str1);
+    // curl_easy_setopt(curl1, CURLOPT_WRITEDATA, &s);
 
-    int pos = strpos1(s.ptr, "ReferenceId\":\"");
+    // int pos = strpos1(s.ptr, "ReferenceId\":\"");
     
 
-    // 8a50fdd4-84cc-11e6-83b4-8e4ab90f4bc9
-    referenceId = (char*)substring1(s.ptr, pos + 14, 36);
-    printf("\nReference Id: %s\n", s.ptr);
+    // // 8a50fdd4-84cc-11e6-83b4-8e4ab90f4bc9
+    // referenceId = (char*)substring1(s.ptr, pos + 14, 36);
+    // printf("\nReference Id: %s\n", s.ptr);
 
-    // authenticated
-    pos = strpos1(s.ptr, "\"Status\":\"");
-    status = substring1(s.ptr, pos + 10, 13);
+    // // authenticated
+    // pos = strpos1(s.ptr, "\"Status\":\"");
+    // status = substring1(s.ptr, pos + 10, 13);
 
-    if (strcmp(status, "authenticated") == 0)
-    {
-      authenticated1 = 1;
-    }
+    // if (strcmp(status, "authenticated") == 0)
+    // {
+    //   authenticated1 = 1;
+    // }
 
-    /* Perform the request, res will get the return code */
-    res1 = curl_easy_perform(curl1);
+    // /* Perform the request, res will get the return code */
+    // res1 = curl_easy_perform(curl1);
 
-    /* Check for errors */
-    if (res1 != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res1));
+    // /* Check for errors */
+    // if (res1 != CURLE_OK)
+    //   fprintf(stderr, "curl_easy_perform() failed: %s\n",
+    //           curl_easy_strerror(res1));
 
-    /* always cleanup */
-    free(s.ptr);
+    // /* always cleanup */
+    // free(s.ptr);
     curl_easy_cleanup(curl1);
   }
   curl_global_cleanup();
