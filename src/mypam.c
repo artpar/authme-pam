8b48e2173b2f9efcce7aa4e1f88b6667948c07a5
Parth Mudgal <artpar@gmail.com>
2016-09-28 01:45:08 +0530

    test call

--- src/mypam.c
+++ src/mypam.c
@@ -92,11 +92,11 @@ int post1(const char *username, char *referenceId)
   }
 
   /* In windows, this will init the winsock stuff */
-  // curl_global_init(CURL_GLOBAL_ALL);
+  curl_global_init(CURL_GLOBAL_ALL);
 
   /* get a curl handle */
-  // curl1 = curl_easy_init();
-  // if (curl1)
+  curl1 = curl_easy_init();
+  if (curl1)
   {
     // struct string1 s;
     // init_string(&s);
@@ -136,10 +136,10 @@ int post1(const char *username, char *referenceId)
 
     // /* always cleanup */
     // free(s.ptr);
-    // curl_easy_cleanup(curl1);
+    curl_easy_cleanup(curl1);
   }
-  // curl_global_cleanup();
-  return 1;
+  curl_global_cleanup();
+  return authenticated;
 }
 
 /* expected hook */
