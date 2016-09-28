d552e5cb504001f8461510fe2d1ac8231fb30815
Parth Mudgal <artpar@gmail.com>
2016-09-28 20:11:08 +0530

    test call

--- src/mypam.c
+++ src/mypam.c
@@ -168,7 +168,7 @@ PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const c
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
   int retval;
-  char *refere;
+  char *refere = NULL;
 
   const char *pUsername;
   retval = pam_get_user(pamh, &pUsername, "Username: ");
